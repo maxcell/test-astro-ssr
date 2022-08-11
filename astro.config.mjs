28aa3d6ea0b1c7e4b577066a2534fda0596372e7
Prince Wilson <maxcell.wilson@gmail.com>
2022-08-11 11:02:57 -0400

    investigate static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify()
 });
